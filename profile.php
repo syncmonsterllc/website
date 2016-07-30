a5c416c2ed2706ecca43f978b066cb2c32a6c772
syncmonster <papp.dany77@icloud.com>
2016-07-29 23:54:59 -0400

    Update 7/29/16
    -added security page
    -added ability to share website on Facebook and Twitter

--- profile.php
+++ profile.php
@@ -3,6 +3,20 @@
 <?php include "profile_nav.php"; ?>
 
 <body id="acc_body">
+    <!--
+        Facebook share button api start
+        -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+    <!--
+    Facebook share button api end
+    -->
     <div id="wrapper">
         <div class="well well-lg">
                 <div class="stor">
@@ -38,9 +52,29 @@
                   </div>
 
                 </div><!-- connected accounts, tiles of cloud accounts, 3x4 grid including thumbnails and rounded progressbars -->
-                <div></div><!-- package type used on this account -->
-                <div></div><!-- invite friends -->
-                <div></div><!-- delete account  -->
+                <div class="col-md-12 pack">
+                  <h4>Package type used</h4>
+                  <p>$Account Username here$</p>
+                  <p>This account is using the basic package. 2GB used of 50GB<a class="btn btn-default" href="upgrade.php">Upgrade</a></p>
+                </div><!-- package type used on this account -->
+
+                <div class="col-md-12 inv">
+                  <h4>Invite friends</h4>
+                  <p>Invite firends and get free space towards your drive. you already invited $number$ friends. Your are rewarded by $number$*$x amound$. </p>
+                  <form class="form-inline col-md-6">
+                    <div class="form-group">
+                      <label for="exampleInputEmail2">Email</label>
+                      <input type="email" class="form-control" placeholder="example@example.com">
+                    </div>
+                    <button type="submit" class="btn btn-default">Send invite</button>
+                  </form>
+                    <div class="col-md-3"><a href="https://twitter.com/share" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+                    <div class="fb-share-button col-md-3" data-href="http://syncmonster.io" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fsyncmonster.io%2F&amp;src=sdkpreparse">Share</a></div>
+                </div><!-- invite friends -->
+                <div class="del col-md-12">
+                  <h4></h4>
+                  <p><a href="">Delete account</a></p>
+                </div><!-- delete account  -->
 
 
         
---
+++ security.php
@@ -0,0 +1,92 @@
+<?php include "header.php"; ?>
+<?php include "profile_nav.php"; ?>
+<?php include "script.php"; ?>
+
+    <body id="acc_body">
+        <div id="wrapper">
+            <div class="well well-lg">
+
+                <h4 class="title">Security</h4>
+                <table class="table table-hover">
+                    <tr>
+                        <th>Device</th>
+                        <th>Location</th>
+                        <th>Date</th>
+                        <th>Severity</th>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-android" aria-hidden="true"></i></td>
+                        <td>Las Vegas, Nevada, USA (IP: 99.184.188.40)</td>
+                        <td>07/25/2016 5:01 GMT</td>
+                        <td>Failed login attempt </td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-firefox" aria-hidden="true"></i></td>
+                        <td>San Francisco, California, USA (IP: 99.184.188.40)</td>
+                        <td>07/10/2015 10:09 GMT</td>
+                        <td class="text-danger">Successful login attempt </td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-edge" aria-hidden="true"></i></td>
+                        <td>Tokyo, Japan (IP: 101.184.188.45)</td>
+                        <td>09/12/2016 7:55 GMT</td>
+                        <td>Failed login attempt </td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-opera" aria-hidden="true"></i></td>
+                        <td>Las Vegas, Nevada, USA (IP: 87.184.188.50)</td>
+                        <td>01/25/2016 22:23 GMT</td>
+                        <td>Failed login attempt </td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-chrome" aria-hidden="true"></i></td>
+                        <td>Washington DC, USA (IP: 42.150.133.25)</td>
+                        <td>07/01/2016 12:35 GMT</td>
+                        <td>Failed login attempt </td>
+                    </tr>
+                    <tr>
+                        <td><i class="fa fa-apple" aria-hidden="true"></i></td>
+                        <td>Huston, Texas, USA (IP: 88.100.100.10)</td>
+                        <td>02/02/2002 2:22 GMT</td>
+                        <td class="text-danger">Successful login attempt </td>
+                    </tr>
+                </table>
+
+                <div class="forgot">
+                    <h4>Change password</h4>
+                    <form>
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <label>Re-Type Password</label>
+                            <input type="password" class="form-control" placeholder="Re-type password">
+                        </div>
+                        <button type="submit" class="btn btn-default">Change password</button>
+                    </form>
+                </div>
+
+                <div class="twostep">
+                    <h4>Two-step verification</h4>
+                    <form>
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label>Phone number</label>
+                            <input type="text" class="form-control" pattern="^([0|\+[0-9]{1,5})?([1-9][0-9]{9})$" placeholder="Phone number" maxlength="10">
+                        </div>
+                        <button type="submit" class="btn btn-default">Set two-step verification up</button>
+                    </form>
+                </div>
+
+
+            </div>
+        </div>
+    </body>
