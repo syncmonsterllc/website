7444d334218e2d607dade2a4fb908f52d8aed839
Daniel Papp <daniel_papp@outlook.com>
2016-08-05 15:45:22 -0400

    small update
    - created settings page basic structure

--- settings.php
+++ settings.php
@@ -5,9 +5,24 @@
 <body id="acc_body">
 <div id="wrapper">
     <div class="well well-lg">
-
-
-
+        <h4>Change e-mail address</h4>
+            <label></label><input type="email"></input>
+        <h4>Change password</h4>
+            <label></label><input type="password"></input>
+        <h4>Upgrade account</h4>
+            <label></label><input type="text"></input>
+        <h4>Change notification frequency</h4>
+            <label></label><input type="checkbox"></input>
+        <h4>Gift pro membership</h4>
+            <label></label><input type="email"></input>
+        <h4>Set up automatic syncronization </h4>
+            <label></label><input type="radio"></input>
+        <h4>Set up avatar</h4>
+            <label></label><input type="file"></input>
+        <h4>Change basic information</h4>
+            <label>Full name</label><input type="text"></input>
+            <label>Date of birth</label><input type="text"></input>
+            <label>Credit Card number</label><input type="text"></input>
     </div>
 </div>
 </body>
